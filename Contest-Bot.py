0cd44b4859aed8574ff150bc0f04bbe58237eff7
Modelmat <modelmat@outlook.com.au>
2017-11-15 23:16:12 +1100

    Added more features

--- Contest-Bot.py
+++ Contest-Bot.py
@@ -1,12 +1,17 @@
 import discord
+import ast
 from discord.ext import commands
 
 '''Add to your server with: https://discordapp.com/oauth2/authorize?client_id=380295391012192256&scope=bot'''
 
 description = '''Allows for the creation of contests that can be used in servers.'''
 bot = commands.Bot(command_prefix='!', description=description)
+Client = discord.Client()
 
 token = open('client_secret.txt', mode='r').read()
+serverSettingsFile = open('serverSettings.txt','r+')
+serverSettings = ast.literal_eval(serverSettingsFile.read())
+serverSettingsFile.close()
 
 @bot.event
 async def on_ready():
@@ -15,9 +20,12 @@ async def on_ready():
     print(bot.user.id)
     print('------')
 
-@bot.command()
-async def submit_contest_item(*args):
+@bot.command(pass_context=True)
+async def submit(ctx, *args):
     """Submits Contest Item"""
+    for server, channelList in serverSettings.items():
+        if ctx.channel.id == server:
+            ctx.chann
     await bot.say(' '.join(args))
 
 bot.run(token)
