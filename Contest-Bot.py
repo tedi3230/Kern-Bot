fd3bb5b6c4e8cd984d082da7068416ef48c03ded
Modelmat <modelmat@outlook.com.au>
2017-11-16 00:25:02 +1100

    Working Version

--- Contest-Bot.py
+++ Contest-Bot.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 
 '''Add to your server with: https://discordapp.com/oauth2/authorize?client_id=380295391012192256&scope=bot'''
 
-description = '''Allows for the creation of contests that can be used in servers.'''
-bot = commands.Bot(command_prefix='!', description=description)
+bot = commands.Bot(command_prefix='!', description='Allows for the creation of contests that can be used in servers.')
 Client = discord.Client()
 
 token = open('client_secret.txt', mode='r').read()
@@ -21,11 +20,15 @@ async def on_ready():
     print('------')
 
 @bot.command(pass_context=True)
-async def submit(ctx, *args):
-    """Submits Contest Item"""
+async def submit(ctx, title, imageURL, *, description):
+    """Submits Contest Item"""    
+    embed = discord.Embed(title="Submission by", description=str(ctx.message.author), color=0x00ff00)
+    embed.add_field(name="Title:", value=title, inline=False)
+    embed.add_field(name="Description:", value=description, inline=False)
+    embed.set_image(url=imageURL)
     for server, channelList in serverSettings.items():
-        if ctx.channel.id == server:
-            ctx.chann
-    await bot.say(' '.join(args))
+        if ctx.message.channel.server.id == server:
+            if ctx.message.channel.id == channelList[1]:
+                await bot.send_message(discord.Object(id=channelList[0]),embed=embed)
 
 bot.run(token)
