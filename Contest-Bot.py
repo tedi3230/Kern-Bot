769b647ccde48e7c29d200a26035794b8544806f
Modelmat <modelmat@outlook.com.au>
2017-11-21 19:31:53 +1100

    Semi-Working, settings added. Using discord.py rewrite

--- Contest-Bot.py
+++ Contest-Bot.py
@@ -3,7 +3,8 @@ import ast
 from discord.ext import commands
 import pickle
 from DB_Access import *
-from atexit import register
+from os import execv
+from sys import argv,executable
 '''Add to your server with: https://discordapp.com/oauth2/authorize?client_id=380598116488970261&scope=bot'''
 
 bot = commands.Bot(command_prefix='!', description='Allows for the creation of contests that can be used in servers.')
@@ -14,75 +15,136 @@ token = tokenFile.read()
 tokenFile.close()
 
 def generateEmbed(messageAuthor,title,colour,description,imageURL,footerText):
-    embed = discord.Embed(title="Submission by:", description=messageAuthor, colour=colour)
+    embed = discord.Embed(title="Submission by:", description=messageAuthor.mention, colour=colour)
     embed.add_field(name="Title:", value=title, inline=False)
     embed.add_field(name="Description:", value=description, inline=False)
     embed.set_image(url=imageURL)
     embed.set_footer(text=footerText)
+    embed.set_thumbnail(url=messageAuthor.avatar_url)
     return embed
 
-@register
-def cleanUp():
-    #await bot.close()
-    Client.close()
-    cleanUP()
+class InvalidParameter(Exception):
+    pass
 
 @bot.event
 async def on_ready():
-    print('Logged in as:')
+    print('\nLogged in as:')
     print(bot.user.name)
     print(bot.user.id)
     print('------')
-    await bot.change_presence(game=discord.Game(name="{} servers for new contests.".format(len(bot.servers)),type=3))
+    await bot.change_presence(game=discord.Game(name="for new contests.",type=3))
 
-@bot.command(pass_context=True)
-async def set_channels(ctx,receiveChannel, allowChannel, outputChannel):
-    """Defines the server channels for use in the submission of contests."""
-    receiveChannelID = receiveChannel.translate({ord(c): None for c in '<>#'})
-    allowChannelID = allowChannel.translate({ord(c): None for c in '<>#'})
-    outputChannelID = outputChannel.translate({ord(c): None for c in '<>#'})
-    setServerChannels(ctx.message.channel.server.id,receiveChannelID, allowChannelID, outputChannelID)
-    await bot.say("Set channels to {} {} {}".format(receiveChannel, allowChannel, outputChannel))
+@bot.command()
+async def restart(ctx):
+    """Owner of this bot only command; Restart the bot"""
+    if ctx.author.id == "310316666171162626":
+        await ctx.send("Restarting Bot.")
+        await bot.logout()
+        execv(executable,['py'] + argv)
 
-@bot.command(pass_context=True)
+@bot.command()
+async def shutdown(ctx):
+    """Owner of this bot only command; Shutdown the bot"""
+    if ctx.author.id == "310316666171162626":
+        await ctx.send("Shutting Down.")
+        await bot.close()
+
+@bot.group(invoke_without_command=True)
+async def settings(ctx):
+    """Change and manage all settings of this bot"""
+    await ctx.send("For commands, type `!help settings set` or `!help settings get ")
+    # no subcommand invoked, just [p]settings
+
+@settings.group(invoke_without_command=True,name="get")
+async def settings_get(ctx):
+    await ctx.send("For commands, type !help settings get")
+
+@settings.group(invoke_without_command=True,name="set")
+async def settings_set(ctx):
+    await ctx.send("For commands, type !help settings set")
+
+@settings_get.command(name="channels")
+async def settings_get_channels(ctx): 
+    channels = getServerChannels(ctx.guild.id,"all")
+    if len(channels) == 3:
+        await ctx.send("​Channels for {}: <#{}>, <#{}>, and <#{}>.".format(ctx.guild.name,channels[0],channels[1],channels[2]))
+    else:
+        print(channels)
+        await ctx.send("​This server does not have channels set up yet, use !settings channels set <receiveChannel> <allowChannel> <outputChannel>.")
+
+@settings_set.command(name="channels")
+async def settings_set_channels(ctx, *args):
+    if len(args) != 4:
+        raise IncorrectNumOfArguments("Too many/little channels supplied, only 3 required.")
+    receiveChannelID = args[0].translate({ord(c): None for c in '<>#'})
+    allowChannelID = args[1].translate({ord(c): None for c in '<>#'})
+    outputChannelID = args[2].translate({ord(c): None for c in '<>#'})
+    setServerChannels(ctx.guild.id,receiveChannelID, allowChannelID, outputChannelID)
+    await ctx.send("​Set channels to {} {} {}".format(args[0], args[1], args[2]))
+
+@settings_set.command(name="prefix")
+async def settings_set_prefix(ctx, prefix):
+    bot = commands.Bot(command_prefix=prefix)
+    setPrefix(ctx.guild.id,prefix)
+    await ctx.send("Set prefix to `{}`".format(prefix))
+
+@settings_get.command(name="prefix")
+async def settings_get_prefix(ctx):
+    await ctx.send("Prefix for {}: `{}`".format(ctx.guild.name,getPrefix(ctx.guild.id)))
+
+@settings.error
+async def settings_error_handler(error,ctx):
+    if isinstance(error,InvalidParameter):
+        ctx.send(str(error))
+    elif isinstance(error,IncorrectNumOfArguments):
+        ctx.send(str(error))
+    else:
+        ctx.send("Warning:\n{}".format(str(error)))
+
+@bot.command()
 async def submit(ctx, title, imageURL, *, description):
     """Submits items into the contest. Enclose title & imageURL in quotes."""
     submissionID = generateID() 
     footerText = "Type !allow {} to allow this and !allow {} False to prevent the moving on this to voting queue.".format(submissionID,submissionID)
-    embed = generateEmbed(ctx.message.author.mention,title,0x00ff00,description,imageURL,footerText)
-    if int(ctx.message.channel.id) == getServerChannels(ctx.message.channel.server.id, "receiveChannelID"):
-        messageID = await bot.send_message(discord.Object(id=getServerChannels(ctx.message.channel.server.id, "allowChannelID")),embed=embed)
+    embed = generateEmbed(ctx.author,title,0x00ff00,description,imageURL,footerText)
+    if int(ctx.channel.id) == getServerChannels(ctx.guild.id, "receiveChannelID"):
+        channel = ctx.guild.get_channel( getServerChannels( ctx.guild.id, "allowChannelID" ) )
+        messageID = await channel.send(embed=embed)
         addSubmission(submissionID,embed,messageID.id)
 
+@submit.error
+async def submit_error_handler(error, ctx):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("You did not pass all the required arguments, please try again.")
+    else:
+        await ctx.send("​Submit Warning:\n%s"%str(error))
 
-# @submit.error
-# async def submit_error_handler(error, ctx):
-#     if isinstance(error, commands.MissingRequiredArgument):
-#         await bot.say("You did not pass all the required arguments, please try again.")
-#     else:
-#         await bot.say("Submit Warning:\n%s"%str(error))
-
-@bot.command(pass_context=True)
+@bot.command()
 async def allow(ctx,submissionID,allowed="True"):
     """Allows for moderators to approve/disaprove submissions."""
     if allowed.lower() == "true":
         embed = getSubmission(submissionID)
         embed.set_footer(text="Type !vote {} 0 to not like it, type !vote {} 5 to really like it.".format(submissionID,submissionID))
-        await bot.send_message(discord.Object(id=getServerChannels(ctx.message.channel.server.id, "outputChannelID")),embed=embed)
+        await ctx.send(embed=embed)
     elif allowed.lower() == "false":
         embed = getSubmission(submissionID)
-        await bot.say("Submssions with submissionID of {} has been disapproved.".format(submissionID))
-        message = await bot.get_message(discord.Object(id=getServerChannels(ctx.message.channel.server.id, "allowChannelID")),getMessageID(submissionID))
+        await ctx.send("​Submssions with submissionID of {} has been disapproved.".format(submissionID))
+        channel = ctx.guild.get_channel( getServerChannels( ctx.guild.id, "allowChannelID" ) )
+        message = await channel.get_message( getServerChannels(ctx.message.guild.id, "allowChannelID") )
         embed.colour = 0xff0000
-        await bot.edit_message(message,embed=embed)
+        await message.edit(embed=embed)
         removeSubmission(submissionID)
     else:
-        await bot.say("A correct value of true/false was not passed ")
+        await ctx.send("​A correct value of true/false was not passed ")
 
-# @allow.error
-# async def allow_error_handler(error, ctx):
-#     if isinstance(error, SubmissionNotExist):
-#         await bot.say(str(error))
-#     else:
-#         await bot.say("Warning:\n%s"%str(error))
-bot.run(token)
+@allow.error
+async def allow_error_handler(error, ctx):
+    if isinstance(error, SubmissionNotExist):
+        await ctx.send(str(error))
+    else:
+        await ctx.send("​Warning:\n%s"%str(error))
+
+try:
+    bot.run(token)
+except (KeyboardInterrupt, EOFError):
+    pass
--- DB_Access.py
+++ DB_Access.py
@@ -6,10 +6,10 @@ dataBase = sqlite3.connect("database.db")
 cur = dataBase.cursor()
 
 cur.execute("CREATE TABLE IF NOT EXISTS Submissions(submissionID INT,Embed MEDIUMBLOB,messageID INT,rating INT)")
-cur.execute("CREATE TABLE IF NOT EXISTS Servers(serverID INT,receiveChannelID INT,allowChannelID INT, outputChannelID INT)")
-channelTypes = ["receiveChannelID","allowChannelID","outputChannelID"]
+cur.execute("CREATE TABLE IF NOT EXISTS Servers(serverID INT,receiveChannelID INT,allowChannelID INT, voteChannelID INT,prefix TEXT)")
+channelTypes = [0,1,2,3]
 
-"""receiveChannelID is where !submit gets sent, allowChannelID is where !allow gets sent, outputChannelID is where the final contestants get sent"""
+"""receiveChannelID is where !submit gets sent, allowChannelID is where !allow gets sent, voteChannelID is where the final contestants get sent"""
 
 class ChannelTypeFailure(Exception):
     pass
@@ -20,23 +20,94 @@ class SubmissionNotExist(Exception):
 class IncorrectNumOfArguments(Exception):
     pass
 
-if __name__ in "__main__":
-    pass
-    #cur.execute("INSERT INTO Servers VALUES(380288809310617600,380289063040712704,380289087657213952,380364317809311746);")
-
 def cleanUP():
     dataBase.close()
 
+
+
+def checkKeyExists(dic,keyList):    
+    values = []
+    fails = []
+    for key in KeyList:
+        if key in dic.keys():
+            values.append(dic[key])
+        else:
+            fails.append(key)
+    if len(key) > 0:
+        tempString = " ".join(fails)
+        raise TypeError("function() missing at least 1 required keyword argument: {}".format(tempString))
+    else:
+        return values
+
+def accessDatabase(aType,table,columns="*",values=tuple(),whereCol=str(),colValues=str()):
+    """Access the database with easy commands.
+    accessDatabase(aType,table,data=None,fields="*",)
+    aType: 0="SELECT",1="INSERT",2="DELETE",3="SELECT WHERE".4="INSERT OR IGNORE" Integer.
+    table = "Table Name". String
+    columns = (col1,col2,etc.). Tuple of strings or "*".
+    values = (value1,value2,etc.). Tuple of strings
+    whereCol = (col). String
+    colValues = (value). String."""
+
+    """Do soemething to detect if those exist (aside from try)"""
+
+    """Access Like So: accessDatabase(1,"Server",values=(col1,col2),colValues="hi
+    
+    Named arguments, argumentName=value
+
+    """"
+
+    values = kwargs['values']
+    whereCol = kwargs['wherCol']
+    colValues = kwargs['colValues']
+    if aType == 0:
+        """SELECT"""
+        checkKeyExists(kwargs,['columns'])
+        cur.execute("SELECT ? FROM ?",(columns,table))
+        return cur.fetchall()
+    elif aType == 1:
+        """INSERT"""
+        if "columns" in kwargs.keys():
+            columns = kwargs['columns']
+        else:
+            
+        values = kwargs['values']
+        if len(columns) != len(values) and columns != "*":
+
+        cur.execute("INSERT INTO ? ? VALUES ?",(table,columns,values))
+    elif aType == 2:
+        """DELETE"""
+        pass
+    elif aType ==3:
+        """SELECT WHERE"""
+        cur.execute("SELECT ? FROM ? WHERE ? = ?",(columns,table,whereCol,colValues))
+        return cur.fetchall()
+    elif aType == 4:
+        """ INSERT OR IGNORE"""
+    else:
+        pass
+        #Fail
+
 def getServerChannels(server, channelType):
+    """Get the submission channels in the server, 0=All Channels,1=Receive Channel",2=Allow Channel,4=Vote Channel"""
     if channelType not in channelTypes:
         raise ChannelTypeFailure("{} is not a valid channelType".format(channelType))
-    cur.execute("SELECT {} FROM Servers WHERE serverID = {}".format(channelType,server))
+    if channelType == 0:
+        cur.execute("SELECT receiveChannelID,allowChannelID,voteChannelID FROM Servers WHERE serverID = (?)",server)
+        accessDatabase(3,)
+        channelIDs = list(cur)
+        channelIDs = list(channelIDs[0])
+        channelIDs.pop(0)
+        return channelIDs
+    else:
+        cur.execute("SELECT {} FROM Servers WHERE serverID = {}".format(channelType,server))
     channelID = cur.fetchall() 
     if len(channelID) == 0:
         raise ServerNotExist("{} is not a known server.".format(server))
     return channelID[0][0]
 
 def setServerChannels(*args):
+    """Set up the channels for making, allowing, and voting for submissions."""
     if len(args) != 4:
         raise IncorrectNumOfArguments("There was an incorrect number of arguments supplied.")
     cur.execute("INSERT OR IGNORE INTO Servers Values(?,?,?,?)",(args[0],args[1],args[2],args[3],))
@@ -44,12 +115,13 @@ def setServerChannels(*args):
     #Make so that serverID & other channels, one has INSERT REPLACe the other is insert IGNORE
 
 def addSubmission(submissionID,embedFile,messageID):
+    """Add to the database the submission, by the submission ID & also including the embed File"""
     dump = pickle.dumps(embedFile)
     cur.execute("INSERT INTO Submissions (submissionID,Embed,messageID) VALUES (?,?,?)",(submissionID,dump,messageID,))
     dataBase.commit()
 
 def getSubmission(submissionID):
-    cur.execute("SELECT Embed FROM Submissions WHERE submissionID = ?",(submissionID,))
+    cur.execute("SELECT Embed FROM Submissions WHERE submissionID = (?)",(submissionID,))
     embed = cur.fetchall()
     if len(embed) == 0:
         raise SubmissionNotExist("{} is not a valid submissionID".format(submissionID))
@@ -57,7 +129,7 @@ def getSubmission(submissionID):
     return pickle.loads(embed)
 
 def removeSubmission(submissionID):
-    cur.execute("DELETE FROM Submissions WHERE submissionID = ?",(submissionID,))
+    cur.execute("DELETE FROM Submissions WHERE submissionID = ?",(submissionID,)) 
     dataBase.commit()
     return None
 
@@ -72,6 +144,22 @@ def generateID():
     return submissionID
 
 def getMessageID(submissionID):
-    cur.execute("SELECT messageID FROM Submissions WHERE submissionID = {}".format(submissionID))
+    cur.execute("SELECT messageID FROM Submissions WHERE submissionID = (?)",submissionID)
     messageID=cur.fetchall()
     return messageID[0][0]
+
+def getPrefix(serverID):
+    cur.execute("SELECT prefix FROM Servers WHERE serverID = (?)",int(serverID))
+    prefix = cur.fetchall()
+    if prefix[0][0] == None:
+        return "!"
+    return prefix[0][0]
+
+def setPrefix(serverID,prefix):
+    cur.execute("INSERT INTO Servers (prefix) VALUES (?);",prefix)
+    dataBase.commit()
+    return None
+
+if __name__ in "__main__":
+    pass
+    #cur.execute("INSERT INTO Servers VALUES(380288809310617600,380289063040712704,380289087657213952,380364317809311746);")
