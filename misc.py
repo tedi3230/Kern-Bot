83ddcedb2fbdf7c2936494bf6943fad4e5e498b7
Modelmat <modelmat@outlook.com.au>
2017-12-04 21:16:23 +1100

    Fix @commands.isowner() issue (added brackets)

--- misc.py
+++ misc.py
@@ -7,7 +7,7 @@ from discord.ext import commands
 
 time_format = '%H:%M:%S UTC on the %d of %B, %Y'
 
-class MiscCog:
+class Misc:
     def __init__(self, bot):
         self.bot = bot
         self.bot_logs = self.bot.get_channel(382780308610744331)
@@ -15,24 +15,26 @@ class MiscCog:
     async def on_guild_join(self, guild):
         self.bot_logs.send("Joined {} at {}".format(guild.name, datetime.utcnow().strftime(time_format)))
 
-    @commands.is_owner
+    @commands.is_owner()
     @commands.command(hidden=True)
     async def restart(self, ctx):
         """Owner of this bot only command; Restart the bot"""
         if ctx.channel != self.bot_logs:
             await ctx.send("Restarting bot.")
         await self.bot_logs.send("Restarting bot.")
+        print("Restarting...")
         await self.bot.close()
         execl(executable, 'python "' + "".join(argv) + '"')
 
-    @commands.is_owner
+    @commands.is_owner()
     @commands.command(hidden=True)
     async def shutdown(self, ctx):
         """Owner of this bot only command; Shutdown the bot"""
         if ctx.channel == self.bot_logs:
             await ctx.send("Shutting Down.")
         await self.bot_logs.send("Shutting down bot.")
+        print("Restarting...")
         await self.bot.close()
 
 def setup(bot):
-    bot.add_cog(MiscCog(bot))
+    bot.add_cog(Misc(bot))
