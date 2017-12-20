2e4e13bb41f1e0cdfdb5b6e99af5fac6b9896d2d
Modelmat <modelmat@outlook.com.au>
2017-12-20 15:29:36 +1100

    made check work

--- settings.py
+++ settings.py
@@ -2,28 +2,27 @@ import discord
 from discord.ext import commands
 import database_old as db
 
+async def settings_perm_check(ctx):
+    if commands.is_owner():
+        return True
+    elif commands.has_permissions(manage_server=True):
+        return True
+    else:
+        await ctx.send("You do not have valid permissions to do this. (Manage Server Permission).")
+        return False
+
 class Settings:
     def __init__(self, bot):
         self.bot = bot
-
-    # async def settings_perm_check(self, ctx):
-    #     if commands.is_owner():
-    #         print("owner")
-    #         return True
-    #     elif commands.has_permissions(manage_server=True):
-    #         print("manage server")
-    #         return True
-    #     else:
-    #         return False
 
     @commands.group()
     async def get(self, ctx):
         await ctx.send("For commands, type _help settings get")
 
-    #@commands.check(settings_perm_check)
+    @commands.check(settings_perm_check)
     @commands.group(name="set")
     async def _set(self, ctx):
-        await ctx.send("For commands, type _help settings set")
+        await ctx.send("For commands, type {}help settings set".format(ctx.prefix))
 
     @get.command(name="channels")
     async def get_channels(self, ctx):
@@ -35,12 +34,12 @@ class Settings:
                                                                                channels[2]))
         else:
             print(channels)
-            await ctx.send("​This server does not have channels set up yet, use c!settings channels set <receiveChannel> <allowChannel> <outputChannel>.")
+            await ctx.send("​This server does not have channels set up yet, use {}settings channels set <receiveChannel> <allowChannel> <outputChannel>.".format(ctx.prefix))
 
     @_set.command(name="channels")
     async def set_channels(self, ctx, *args):
         if len(args) < 3:
-            raise TypeError("Too few channels supplied, you need three. Type c!help settings set channels for more inforamtion")
+            raise TypeError("Too few channels supplied, you need three. Type {}help settings set channels for more inforamtion".format(ctx.prefix))
         print(args)
         receiveChannelID = args[0].translate({ord(c): None for c in '<>#'})
         allowChannelID = args[1].translate({ord(c): None for c in '<>#'}) #UPDATE FOR NEW SYNTAX
@@ -49,7 +48,8 @@ class Settings:
         await ctx.send("​Set channels to {} {} {}".format(args[0], args[1], args[2]))
 
     @_set.command(name="prefix")
-    async def set_prefix(self, ctx, prefix):
+    async def set_prefix(self, ctx, *, prefix: str):
+        prefix = prefix.strip("'").strip('"')
         if db.set_prefix(ctx.guild.id, prefix): 
             await ctx.send("Channels are not set. Currently a limitation.")
         await ctx.send("Set prefix to `{}`".format(db.get_prefix(ctx.guild.id)))
