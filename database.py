4ffb6909628c202a2efdd68a246555bedfbe21c7
Modelmat <Modelmat@users.noreply.github.com>
2018-01-06 13:06:25 +1100

    switched to using `await self.pool.acquire()` to close connections

--- database.py
+++ database.py
@@ -35,6 +35,7 @@ servers_table = """
 class Database:
     """Accessing database functions"""
     def __init__(self, bot):
+        self.bot = bot
         try:
             self.dsn = os.environ["DATABASE_URL"]
         except KeyError:
@@ -85,27 +86,27 @@ class Database:
                     SET receive_channel_id = excluded.receive_channel_id,
                         allow_channel_id = excluded.allow_channel_id,
                         vote_channel_id = excluded.vote_channel_id;"""
-
-        await self.pool.execute(sql, server_id, *channels)
+        async with self.pool.acquire() as con:
+            await con.execute(sql, server_id, *channels)
 
     async def get_contest_channels(self, server_id: int):
         sql = """SELECT receive_channel_id, allow_channel_id, vote_channel_id FROM servers
                  WHERE server_id = $1"""
-
-        return await self.pool.fetchrow(sql, server_id)
+        async with self.pool.acquire() as con:
+            channels = await con.fetchrow(sql, server_id)
+        return channels
 
     async def set_prefix(self, server_id: int, prefix: str):
         sql = """INSERT INTO servers(server_id, prefix) VALUES ($2, $1)
                  ON CONFLICT (server_id) DO UPDATE
                     SET prefix = $1"""
-
-        await self.pool.execute(sql, prefix, server_id)
+        async with self.pool.acquire() as con:
+            await con.execute(sql, prefix, server_id)
         return prefix
 
     async def get_prefix(self, server_id: int):
-        prefix = await self.pool.fetchval("SELECT prefix FROM servers WHERE server_id = $1", server_id)
-        if prefix is None:
-            return "k "
+        async with self.pool.acquire() as con:
+            prefix = await con.fetchval("SELECT prefix FROM servers WHERE server_id = $1", server_id) or self.bot.prefix
         return prefix
 
     async def remove_prefix(self, server_id: int):
@@ -113,20 +114,25 @@ class Database:
             await con.execute("UPDATE servers SET prefix = NULL WHERE server_id = $1", server_id)
 
     async def add_contest_submission(self, server_id: int, owner_id: int, submission_id: int, embed: discord.Embed):
-        print(embed.to_dict())
-        await self.pool.execute("""INSERT INTO submissions (submission_id, owner_id, embed, server_id) VALUES ($1, $2, $3, $4)""",
-                                submission_id, owner_id, json.dumps(embed.to_dict()), server_id)
+        async with self.pool.acquire() as con:
+            await con.execute("""INSERT INTO submissions (submission_id, owner_id, embed, server_id) VALUES ($1, $2, $3, $4)""",
+                              submission_id, owner_id, json.dumps(embed.to_dict()), server_id)
 
     async def get_contest_submission(self, submission_id: int):
+        async with self.pool.acquire() as con:
+            embed = await con.fetchval("SELECT embed FROM submissions WHERE submission_id = $1", submission_id)
         embed = await self.pool.fetchval("SELECT embed FROM submissions WHERE submission_id = $1", submission_id)
         return discord.Embed.from_data(json.loads(embed))
 
     async def list_contest_submissions(self, server_id: int):
-        return await self.pool.fetch("SELECT submission_id, embed FROM submissions WHERE server_id = $1",
-                              server_id)
+        async with self.pool.acquire() as con:
+            submissions = await con.fetch("SELECT submission_id, embed FROM submissions WHERE server_id = $1",
+                                      server_id)
+        return submissions
 
     async def remove_contest_submission(self, server_id: int, owner_id: int, submission_id: int):
-        await self.pool.execute('DELETE FROM submissions WHERE submission_id = $1 AND owner_id = $2 AND server_id = $3', submission_id, owner_id, server_id)
+        async with self.pool.acquire() as con:
+            await con.execute('DELETE FROM submissions WHERE submission_id = $1 AND owner_id = $2 AND server_id = $3', submission_id, owner_id, server_id)
 
 if __name__ in '__main__':
     db_hi = Database('lol')
