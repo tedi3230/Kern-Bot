2703c6cc80ef6c3ec0f8d60baede107ac7c39b68
Modelmat <modelmat@outlook.com.au>
2018-01-08 10:20:30 +1100

    added bot class with parameters defined.

--- custom_classes.py
+++ custom_classes.py
@@ -1,14 +1,41 @@
 from urllib.parse import urlparse
+from datetime import datetime
 import discord
 from discord.ext import commands
+
+async def bot_user_check(ctx):
+    return not ctx.author.bot
+
+class Bot(commands.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.database = None
+        self.prefix = "k"
+        self.server_prefixes = {}
+        self.time_format = '%H:%M:%S UTC on the %d of %B, %Y'
+        self.bot_logs_id = 382780308610744331
+        self.launch_time = datetime.utcnow()
+        super().add_check(bot_user_check)
+        self.todo = """TODO: ```
+                    01. Finish contests cog
+                    02. Use logging module
+                    03. Fix the error with overload in define function (total = 15)
+                    04. Finish and neaten up the help command (possibly use  HelpFormatter). Add docstrings to all commands
+                    05. Hack Command - add fake attack
+                    06. Server rules (database - rules) - custom titles
+                    07. Custom context
+                    08. Stackexchange search - https://api.stackexchange.com/docs
+                    09. Make prefixes a list (for multiple)
+                    ```
+                    """
+
+    class ResponseError(Exception):
+        pass
 
 class CustomContext(commands.Context):
     def clean_prefix(self):
         user = self.bot.user
-        prefix = self.prefix.replace(user.mention, '@' + user.name + " `")
-        print(prefix)
-        if prefix == self.prefix:
-            return "`" + self.prefix
+        prefix = self.prefix.replace(user.mention, '@' + user.name)
         return prefix
     async def error(self, error, title="Error:", channel: discord.TextChannel = None, *args, **kwargs):
         error_embed = discord.Embed(title=title, colour=0xff0000, description=f"{error}")
@@ -21,9 +48,6 @@ class CustomContext(commands.Context):
         if channel is None:
             return await super().send(embed=success_embed, *args, **kwargs)
         return await channel.send(embed=success_embed, *args, **kwargs)
-
-class ResponseError(Exception):
-    pass
 
 class FakeChannel:
     def __init__(self, name):
