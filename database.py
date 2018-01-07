5dd56eb448d16eb260a05fc9e656d62dd47b5f1c
Modelmat <modelmat@outlook.com.au>
2018-01-08 10:25:17 +1100

    simplifying many of the commands. also, using `async with self.lock`

--- database.py
+++ database.py
@@ -58,9 +58,8 @@ class Database:
         ssl_object = ssl.create_default_context()
         ssl_object.check_hostname = False
         ssl_object.verify_mode = ssl.CERT_NONE
-        await self.lock.acquire()
-        self.pool = await asyncpg.create_pool(self.dsn, ssl=ssl_object)
-        self.lock.release()
+        async with self.lock:
+            self.pool = await asyncpg.create_pool(self.dsn, ssl=ssl_object)
         async with self.pool.acquire() as con:
             if not await con.fetch("SELECT relname FROM pg_class WHERE relname = 'servers'"):
                 await con.execute(servers_table)
@@ -77,7 +76,7 @@ class Database:
             submission_id = "{:06}".format(randint(0, 999999))
         return int(submission_id)
 
-    async def set_contest_channels(self, server_id: int, *channels):
+    async def set_contest_channels(self, ctx, *channels):
         sql = """INSERT INTO servers (server_id, receive_channel_id, allow_channel_id, vote_channel_id)
                  VALUES ($1, $2, $3, $4)
                  ON CONFLICT (server_id) DO UPDATE
@@ -85,52 +84,55 @@ class Database:
                         allow_channel_id = excluded.allow_channel_id,
                         vote_channel_id = excluded.vote_channel_id;"""
         async with self.pool.acquire() as con:
-            await con.execute(sql, server_id, *channels)
+            await con.execute(sql, ctx.guild.id, *channels)
 
-    async def get_contest_channels(self, server_id: int):
+    async def get_contest_channels(self, ctx):
         sql = """SELECT receive_channel_id, allow_channel_id, vote_channel_id FROM servers
                  WHERE server_id = $1"""
         async with self.pool.acquire() as con:
-            channels = await con.fetchrow(sql, server_id)
+            channels = await con.fetchrow(sql, ctx.guild.id)
         return channels
 
-    async def set_prefix(self, server_id: int, prefix: str):
+    async def set_prefix(self, ctx, prefix: str):
         sql = """INSERT INTO servers(server_id, prefix) VALUES ($2, $1)
                  ON CONFLICT (server_id) DO UPDATE
                     SET prefix = $1"""
         async with self.pool.acquire() as con:
-            await con.execute(sql, prefix, server_id)
+            await con.execute(sql, prefix, ctx.guild.id)
         return prefix
 
-    async def get_prefix(self, server_id: int):
+    async def get_prefix(self, ctx):
         async with self.pool.acquire() as con:
-            prefix = await con.fetchval("SELECT prefix FROM servers WHERE server_id = $1", server_id) or self.bot.prefix
+            prefix = await con.fetchval("SELECT prefix FROM servers WHERE server_id = $1", ctx.guild.id) or self.bot.prefix
         return prefix
 
-    async def remove_prefix(self, server_id: int):
+    async def remove_prefix(self, ctx):
         async with self.pool.acquire() as con:
-            await con.execute("UPDATE servers SET prefix = NULL WHERE server_id = $1", server_id)
+            await con.execute("UPDATE servers SET prefix = NULL WHERE server_id = $1", ctx.guild.id)
 
-    async def add_contest_submission(self, server_id: int, owner_id: int, submission_id: int, embed: discord.Embed):
+    async def add_contest_submission(self, ctx, embed: discord.Embed):
         async with self.pool.acquire() as con:
             await con.execute("""INSERT INTO submissions (server_id, owner_id, submission_id, embed) VALUES ($1, $2, $3, $4)""",
-                              server_id, owner_id, submission_id, json.dumps(embed.to_dict()))
+                              ctx.guild.id, ctx.author.id, self.generate_id(), json.dumps(embed.to_dict()))
 
     async def get_contest_submission(self, submission_id: int):
         async with self.pool.acquire() as con:
             embed = await con.fetchval("SELECT embed FROM submissions WHERE submission_id = $1", submission_id)
-        embed = await self.pool.fetchval("SELECT embed FROM submissions WHERE submission_id = $1", submission_id)
         return discord.Embed.from_data(json.loads(embed))
 
-    async def list_contest_submissions(self, server_id: int):
+    async def list_contest_submissions(self, ctx):
         async with self.pool.acquire() as con:
             submissions = await con.fetch("SELECT owner_id, submission_id, embed FROM submissions WHERE server_id = $1",
-                                      server_id)
+                                          ctx.guild.id)
         return submissions
 
-    async def remove_contest_submission(self, server_id: int, owner_id: int, submission_id: int):
+    async def remove_contest_submission(self, ctx):
         async with self.pool.acquire() as con:
-            await con.execute('DELETE FROM submissions WHERE submission_id = $1 AND owner_id = $2 AND server_id = $3', submission_id, owner_id, server_id)
+            await con.execute('DELETE FROM submissions WHERE owner_id = $1 AND server_id = $2', ctx.author.id, ctx.guild.id)
+
+    async def clear_contest_submission(self, ctx, owner_id):
+        async with self.pool.acquire() as con:
+            await con.execute('DELETE FROM submissions WHERE owner_id = $1 AND server_id = $2', owner_id, ctx.guild.id)
 
 if __name__ in '__main__':
     db_hi = Database('lol')
