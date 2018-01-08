f32589c70debcc89a84f43f9aa0635da0bd27e7f
Modelmat <modelmat@outlook.com.au>
2018-01-08 20:59:37 +1100

    removed weird extra foramt strings (no need); added neutral class (which is not used)

--- custom_classes.py
+++ custom_classes.py
@@ -38,16 +38,22 @@ class CustomContext(commands.Context):
         prefix = self.prefix.replace(user.mention, '@' + user.name)
         return prefix
     async def error(self, error, title="Error:", channel: discord.TextChannel = None, *args, **kwargs):
-        error_embed = discord.Embed(title=title, colour=0xff0000, description=f"{error}")
+        error_embed = discord.Embed(title=title, colour=0xff0000, description=error)
         if channel is None:
             return await super().send(embed=error_embed, *args, **kwargs)
         return await channel.send(embed=error_embed, *args, **kwargs)
 
     async def success(self, success, title="Success", channel: discord.TextChannel = None, *args, **kwargs):
-        success_embed = discord.Embed(title=title, colour=0x00ff00, description=f"{success}")
+        success_embed = discord.Embed(title=title, colour=0x00ff00, description=success)
         if channel is None:
             return await super().send(embed=success_embed, *args, **kwargs)
         return await channel.send(embed=success_embed, *args, **kwargs)
+
+    async def neutral(self, text, title, channel: discord.TextChannel = None, *args, **kwargs):
+        neutral_embed = discord.Embed(title=title, colour=discord.Colour.blurple(), description=text)
+        if channel is None:
+            return await super().send(embed=neutral_embed, *args, **kwargs)
+        return await channel.send(embed=neutral_embed, *args, **kwargs)
 
 class FakeChannel:
     def __init__(self, name):
