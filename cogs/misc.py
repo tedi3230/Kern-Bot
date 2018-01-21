d2ea05d79765f6c1f1626809f023cf4a8cdab6ff
Modelmat <Modelmat@users.noreply.github.com>
2018-01-22 10:12:36 +1100

    fixed embed text to just say the number

--- cogs/misc.py
+++ cogs/misc.py
@@ -72,11 +72,8 @@ class Misc:
         raw = pattern.sub(replace, msg.content)
         embed_text = str()
         if msg.embeds:
-            embed_text += "*There are {} embeds attached;*\n".format(len(msg.embeds))
-            for e in msg.embeds:
-                embed_text += f"*{e.title}*, "
-            embed_text = embed_text[:-2]
-        embed = discord.Embed(description=f"​\n​\n{raw}\n​\n​" + embed_text, timestamp=datetime.utcnow(), colour=discord.Colour.blurple())
+            embed_text += "*There are {} embeds attached;*".format(len(msg.embeds))
+        embed = discord.Embed(description=f"​\n{raw}\n​" + embed_text, timestamp=datetime.utcnow(), colour=discord.Colour.blurple())
         embed.set_footer(text="Requested by: {}".format(ctx.message.author), icon_url=ctx.message.author.avatar_url)
         embed.set_author(name="Message by: {}".format(msg.author), icon_url=msg.author.avatar_url)
         await ctx.send(embed=embed)
