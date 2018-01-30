b83c9d1fee0f06dede4eeef7db309534efd430ff
Modelmat <Modelmat@users.noreply.github.com>
2018-01-30 18:03:23 +1100

    add __error, and alreadysubmitted error

--- cogs/contests.py
+++ cogs/contests.py
@@ -1,7 +1,13 @@
 import json
+import traceback
 import asyncio
 import discord
 from discord.ext import commands
+
+
+class AlreadySubmitted(Exception):
+    pass
+
 
 async def manage_server_check(ctx):
     if commands.is_owner():
@@ -11,11 +17,24 @@ async def manage_server_check(ctx):
     await ctx.error("You do not have valid permissions to do this. (Manage Server Permission).", "Permissions Error")
     return False
 
+
 class Contests:
     """Contest functions"""
+
     def __init__(self, bot):
         self.bot = bot
         self.bot_logs = self.bot.get_channel(bot.bot_logs_id)
+
+    async def __error(self, ctx, error):
+        error = getattr(error, 'original', error)
+        if isinstance(error, (TypeError, ValueError, AlreadySubmitted)):
+            await ctx.error(error)
+        else:
+            print('Ignoring {} in command {}'.format(
+                type(error).__qualname__, ctx.command))
+            traceback.print_exception(type(error), error, error.__traceback__)
+            await ctx.error("An undhandled exception occurred. Don't worry, we know about it now. :thumbsup:")
+            await ctx.error("```{}: {}```".format(type(error).__qualname__, error), title=f"Ignoring exception in command *{ctx.command}*:", channel=self.bot_logs, rqst_by=False)
 
     #pylint: disable-msg=too-many-arguments
     def generateEmbed(self, message_author: discord.User, title, description, image_url=None, colour=0x00ff00):
@@ -34,8 +53,10 @@ class Contests:
         Returns:
             [discord.Embed] -- The embed object generated.
         """
-        embed = discord.Embed(title=title, description=description, colour=colour)
-        embed.set_author(name=f"Author: {message_author.display_name}", icon_url=message_author.avatar_url)
+        embed = discord.Embed(
+            title=title, description=description, colour=colour)
+        embed.set_author(
+            name=f"Author: {message_author.display_name}", icon_url=message_author.avatar_url)
         if image_url is not None:
             embed.set_image(url=image_url)
         embed.set_thumbnail(url=message_author.avatar_url)
@@ -47,24 +68,28 @@ class Contests:
         """Submits an item into a contest. {}submit <title> | <description> | [imageURL]. Note the spaces"""
         input_split = tuple(args.split(" | "))
         if len(input_split) == 1:
-            raise TypeError("submit missing 2 required positional arguments: 'description' and 'image_url'")
+            raise TypeError(
+                "submit missing 2 required positional arguments: 'description' and 'image_url'")
         elif len(input_split) > 3:
-            raise TypeError("submit takes 3 positional arguments but {} were given".format(len(input_split)))
+            raise TypeError(
+                "submit takes 3 positional arguments but {} were given".format(len(input_split)))
         title, description = input_split[0:2]
         if len(input_split) == 3:
             image_url = input_split[2]
         else:
             image_url = ""
-        embed = self.generateEmbed(ctx.author, title, description, image_url, 0x00ff00)
+        embed = self.generateEmbed(
+            ctx.author, title, description, image_url, 0x00ff00)
         server_channels = await self.bot.database.get_contest_channels(ctx)
         if server_channels is None:
             return await ctx.error(f"No server channels are configured. Use {ctx.prefix}set channels to set your channels", title="Configuration Error:")
         if ctx.channel.id == server_channels[0]:
             channel = ctx.guild.get_channel(server_channels[1])
-            if ctx.author.id in [sub['owner_id'] for sub in await self.bot.database.list_contest_submissions(ctx)]:
-                return await ctx.error("You already have a contest submitted. To change your submission, delete it and resubmit.", "Error submitting:")
+            # if ctx.author.id in [sub['owner_id'] for sub in await self.bot.database.list_contest_submissions(ctx)]:
+            #    return await ctx.error("You already have a contest submitted. To change your submission, delete it and resubmit.", "Error submitting:")
             submission_id = await self.bot.database.add_contest_submission(ctx, embed)
-            footer_text = "Type `{0}vote {1} X` to vote for this. X is a number".format(ctx.prefix, submission_id)
+            footer_text = "Type `{0}vote {1} X` to vote for this. X is a number".format(
+                ctx.prefix, submission_id)
             embed.set_footer(text=footer_text)
             await channel.send(embed=embed)
             if ctx.channel.id != server_channels[1]:
@@ -84,9 +109,9 @@ class Contests:
             s_id = submission['submission_id']
             author = ctx.guild.get_member(submission['owner_id'])
             rating = submission['rating'] or "NIL"
-            compiled += f"{index + 1}). **{embed.fields[0].value}** by {author.mention} [id: {s_id}]. **Rating:** `{rating}` points.\n"
+            compiled += f"{index + 1}). **{embed.title}** by {author.mention} [id: {s_id}]. **Rating:** `{rating}` points.\n"
         max_points = await self.bot.database.get_max_rating(ctx)
-        await ctx.neutral(compiled, f"Submissions for {ctx.guild} [/{max_points}]")
+        await ctx.neutral(compiled, f"Submissions Leaderboard for {ctx.guild} [/{max_points}]")
         return [submission['submission_id'] for submission in submissions]
 
     @commands.command()
@@ -116,6 +141,7 @@ class Contests:
         if length == 0:
             return await ctx.error(f"The server `{ctx.guild.name}` has no contest submission, so you cannot purge them.", "No submissions to purge:")
         await ctx.send("Are you sure? [Y/n] This deletes {} submissions".format(length))
+
         def check(m):
             return m.author == ctx.author
         try:
@@ -126,6 +152,7 @@ class Contests:
             return await ctx.send("Ok! Cancelling purge.")
         await self.bot.database.purge_contest_submissions(ctx)
         await ctx.success("{} submission(s) has been successfully purged.".format(length))
+
 
 def setup(bot):
     bot.add_cog(Contests(bot))
