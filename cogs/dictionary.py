68927ad738a2cfda5400e381366469f265b1b539
Modelmat <Modelmat@users.noreply.github.com>
2018-02-11 14:29:48 +1100

    using async with ctx.typing() for long downloads

--- cogs/dictionary.py
+++ cogs/dictionary.py
@@ -16,7 +16,7 @@ class Dictionary:
     def __init__(self, bot):
         self.bot = bot
         self.bot_logs = self.bot.get_channel(bot.bot_logs_id)
-        self.dictionary_base_URL = 'https://od-api.oxforddictionaries.com/api/v1/entries/en/{}'
+        self.dictionary_base_url = 'https://od-api.oxforddictionaries.com/api/v1/entries/en/{}'
         self.image_base_url = 'https://duckduckgo.com/'
         try:
             app_id = environ["APP_ID"]
@@ -87,122 +87,122 @@ class Dictionary:
     @commands.command(aliases=['synonyms', 's'])
     async def synonym(self, ctx, *, term):
         """Return an embed of synonyms for the word passed."""
-        await ctx.trigger_typing()
-        data = await self._get_dic_request(self.dictionary_base_URL.format(term.lower()) + "/synonyms")
-        if data is None:
-            await ctx.send(embed=await self._word_not_found(term))
-            return
+        async with ctx.typing():
+            data = await self._get_dic_request(self.dictionary_base_url.format(term.lower()) + "/synonyms")
+            if data is None:
+                await ctx.send(embed=await self._word_not_found(term))
+                return
 
-        lexical_entries = data[0]['lexicalEntries']
-        lexicon = dict()
-        for entry in lexical_entries:
-            lexicon[entry['lexicalCategory']] = [synonym['text']
-                                                 for synonym in entry['entries'][0]['senses'][0]['synonyms']]
-        embed = discord.Embed(title="Synonyms: {}".format(term.capitalize(
-        ), colour=0x00ff00, url='https://en.oxforddictionaries.com/thesaurus/{}'.format(term)))
-        for category, synonyms in lexicon.items():
-            synonym_list = ["[{}](https://en.oxforddictionaries.com/definition/{})".format(
-                term.capitalize(), "_".join(term.split())) for term in synonyms[:5:]]
-            if len(synonym_list) > 1:
-                category += "s:"
-            embed.add_field(name=category.capitalize(),
-                            value="\n".join(synonym_list))
-        embed.set_author(name="Antonyms for: {}".format(
-            term), url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
+            lexical_entries = data[0]['lexicalEntries']
+            lexicon = dict()
+            for entry in lexical_entries:
+                lexicon[entry['lexicalCategory']] = [synonym['text']
+                                                     for synonym in entry['entries'][0]['senses'][0]['synonyms']]
+            embed = discord.Embed(title="Synonyms: {}".format(term.capitalize(
+            ), colour=0x00ff00, url='https://en.oxforddictionaries.com/thesaurus/{}'.format(term)))
+            for category, synonyms in lexicon.items():
+                synonym_list = ["[{}](https://en.oxforddictionaries.com/definition/{})".format(
+                    term.capitalize(), "_".join(term.split())) for term in synonyms[:5:]]
+                if len(synonym_list) > 1:
+                    category += "s:"
+                embed.add_field(name=category.capitalize(),
+                                value="\n".join(synonym_list))
+            embed.set_author(name="Antonyms for: {}".format(
+                term), url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
     @commands.command(aliases=['antonyms', 'a'])
     async def antonym(self, ctx, *, term):
         """Return an embed of antonyms for the word passed."""
-        await ctx.trigger_typing()
-        data = await self._get_dic_request(self.dictionary_base_URL.format(term.lower()) + "/antonyms")
-        if data is None:
-            await ctx.send(embed=await self._word_not_found(term))
-            return
+        async with ctx.typing():
+            data = await self._get_dic_request(self.dictionary_base_url.format(term.lower()) + "/antonyms")
+            if data is None:
+                await ctx.send(embed=await self._word_not_found(term))
+                return
 
-        lexical_entries = data[0]['lexicalEntries']
-        lexicon = dict()
-        for entry in lexical_entries:
-            lexicon[entry['lexicalCategory']] = [antonym['text']
-                                                 for antonym in entry['entries'][0]['senses'][0]['antonyms']]
-        embed = discord.Embed(title="antonyms: {}".format(term.capitalize(
-        ), colour=0x00ff00, url='https://en.oxforddictionaries.com/thesaurus/{}'.format(term)))
-        for category, antonyms in lexicon.items():
-            antonym_list = ["[{}](https://en.oxforddictionaries.com/definition/{})".format(
-                term.capitalize(), "_".join(term.split())) for term in antonyms[:5:]]
-            if len(antonym_list) > 1:
-                category += "s:"
-            embed.add_field(name=category.capitalize(),
-                            value="\n".join(antonym_list))
-        embed.set_author(name="Antonyms for: {}".format(
-            term), url='https://en.oxforddictionaries.com/thesaurus/{}'.format("_".join(term.split())))
+            lexical_entries = data[0]['lexicalEntries']
+            lexicon = dict()
+            for entry in lexical_entries:
+                lexicon[entry['lexicalCategory']] = [antonym['text']
+                                                     for antonym in entry['entries'][0]['senses'][0]['antonyms']]
+            embed = discord.Embed(title="antonyms: {}".format(term.capitalize(
+            ), colour=0x00ff00, url='https://en.oxforddictionaries.com/thesaurus/{}'.format(term)))
+            for category, antonyms in lexicon.items():
+                antonym_list = ["[{}](https://en.oxforddictionaries.com/definition/{})".format(
+                    term.capitalize(), "_".join(term.split())) for term in antonyms[:5:]]
+                if len(antonym_list) > 1:
+                    category += "s:"
+                embed.add_field(name=category.capitalize(),
+                                value="\n".join(antonym_list))
+            embed.set_author(name="Antonyms for: {}".format(
+                term), url='https://en.oxforddictionaries.com/thesaurus/{}'.format("_".join(term.split())))
 
-        await ctx.send(embed=embed)
+            await ctx.send(embed=embed)
 
     @commands.command(aliases=['meaning', 'd'])
     async def define(self, ctx, *, term):
         """Return an embed of definitions for the word passed. Includes image and more."""
-        await ctx.trigger_typing()
-        data = await self._get_dic_request(self.dictionary_base_URL.format(term.lower()))
-        if data is None:
-            await ctx.send(embed=await self._word_not_found(term))
-            return
+        async with ctx.typing():
+            data = await self._get_dic_request(self.dictionary_base_url.format(term.lower()))
+            if data is None:
+                await ctx.send(embed=await self._word_not_found(term))
+                return
 
-        results = data[0]['lexicalEntries']
-        category_list = {}
+            results = data[0]['lexicalEntries']
+            category_list = {}
 
-        async for definition in self._result_parser(results):
-            if definition[0] in category_list:
-                category_list[definition[0]].append(definition)
+            async for definition in self._result_parser(results):
+                if definition[0] in category_list:
+                    category_list[definition[0]].append(definition)
+                else:
+                    category_list[definition[0]] = [definition]
+
+            embed = discord.Embed(
+                colour=0x00ff00, url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
+            for lexical_category, base_definitions in category_list.items():
+                value = ""
+                print(len(base_definitions), type(base_definitions))
+                if len(base_definitions) > 5:
+                    sample_size = 5
+                else:
+                    sample_size = len(base_definitions)
+                definitions = sample(base_definitions, sample_size)
+                for definition in base_definitions:
+                    for domain in definition[1]:
+                        value += "*{}*, ".format(domain)
+                    mean = definition[2]
+                    if mean:
+                        value += definition[2][0].capitalize()
+                    for example in definition[3]:
+                        value += "\n*{}.*".format(example['text'].capitalize())
+                    value += "\n\n"
+                embed.add_field(name=lexical_category, value=value, inline=False)
+
+            ipa_string = str()
+            for entry in results:
+                if 'pronunications' in entry:
+                    for pronunciation in entry['pronunciations']:
+                        ipa_string += "**{}:** {}\n".format(
+                            entry['lexicalCategory'], pronunciation['phoneticSpelling'])
+                    embed.add_field(name="Pronunciation", value=ipa_string)
+
+            if 'etymologies' in data[0]["lexicalEntries"][0]['entries'][0]:
+                etymology = data[0]["lexicalEntries"][0]['entries'][0]['etymologies'][0]
+                embed.add_field(name="Word Origin:", value=etymology)
+
+            if len(category_list) == 1 and next(iter(category_list)) == "Residual":
+                url_term = term.upper()
             else:
-                category_list[definition[0]] = [definition]
+                url_term = term.capitalize()
 
-        embed = discord.Embed(
-            colour=0x00ff00, url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
-        for lexical_category, base_definitions in category_list.items():
-            value = ""
-            print(len(base_definitions), type(base_definitions))
-            if len(base_definitions) > 5:
-                sample_size = 5
-            else:
-                sample_size = len(base_definitions)
-            definitions = sample(base_definitions, sample_size)
-            for definition in base_definitions:
-                for domain in definition[1]:
-                    value += "*{}*, ".format(domain)
-                mean = definition[2]
-                if len(mean) > 0:
-                    value += definition[2][0].capitalize()
-                for example in definition[3]:
-                    value += "\n*{}.*".format(example['text'].capitalize())
-                value += "\n\n"
-            embed.add_field(name=lexical_category, value=value, inline=False)
+            embed.set_author(name="Definition for: {}".format(
+                url_term), url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
 
-        ipa_string = str()
-        for entry in results:
-            if 'pronunications' in entry:
-                for pronunciation in entry['pronunciations']:
-                    ipa_string += "**{}:** {}\n".format(
-                        entry['lexicalCategory'], pronunciation['phoneticSpelling'])
-                embed.add_field(name="Pronunciation", value=ipa_string)
-
-        if 'etymologies' in data[0]["lexicalEntries"][0]['entries'][0]:
-            etymology = data[0]["lexicalEntries"][0]['entries'][0]['etymologies'][0]
-            embed.add_field(name="Word Origin:", value=etymology)
-
-        if len(category_list) == 1 and next(iter(category_list)) == "Residual":
-            url_term = term.upper()
-        else:
-            url_term = term.capitalize()
-
-        embed.set_author(name="Definition for: {}".format(
-            url_term), url='https://en.oxforddictionaries.com/definition/{}'.format("_".join(term.split())))
-
-        embed.set_thumbnail(url=await self._get_image(term))
-        embed.set_footer(text="Requested by: {} • Results provided by the Oxford Dictionary".format(
-            ctx.message.author), icon_url='https://en.oxforddictionaries.com/apple-touch-icon-180x180.png')
-        await ctx.send(embed=embed)
+            embed.set_thumbnail(url=await self._get_image(term))
+            embed.set_footer(text="Requested by: {} • Results provided by the Oxford Dictionary".format(
+                ctx.message.author), icon_url='https://en.oxforddictionaries.com/apple-touch-icon-180x180.png')
+            await ctx.send(embed=embed)
 
 
 def setup(bot):
