45ab51b23eb77b7ce3eae7670a06e87ba12e7748
Modelmat <Modelmat@users.noreply.github.com>
2018-02-14 17:49:23 +1100

    adding starts

---
+++ cogs/statistics.py
@@ -0,0 +1,24 @@
+import io
+import matplotlib.pyplot as plt
+
+import discord
+from discord.ext import commands
+
+class Statistics:
+    def __init__(self, bot):
+        self.bot = bot
+
+    # async def uh(self, ctx):
+    #     plt.figure()
+    #     plt.plot([1, 2])
+    #     plt.title("test")
+    #     buf = io.BytesIO()
+    #     plt.savefig(buf, format='png')
+
+    #     f = discord.File(buf, filename="image.png")
+    #     e = discord.Embed(image_url="attachment://image.png")
+    #     await messagable.send(file=f, embed=e)
+
+
+def setup(bot):
+    bot.add_cog(Statistics(bot))
