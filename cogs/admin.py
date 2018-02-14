df06c0d0b3069ad0ca9dec258a122cc41a869acc
Modelmat <Modelmat@users.noreply.github.com>
2018-02-14 17:49:15 +1100

    changing looks

--- cogs/admin.py
+++ cogs/admin.py
@@ -51,8 +51,8 @@ class Admin:
     async def rebirth(self, ctx):
         """Owner of this bot only command; Restart the bot"""
         if ctx.channel != self.bot_logs:
-            await ctx.success(datetime.utcnow().strftime(self.bot.time_format), "Restarting:", rqst_by=False, timestamp=False)
-        await ctx.success(datetime.utcnow().strftime(self.bot.time_format), "Restarting:", channel=self.bot_logs, rqst_by=False, timestamp=False)
+            await ctx.success(None, f"Restarting @ {datetime.utcnow().strftime('%H:%M:%s')}", rqst_by=False)
+        await ctx.success(None, f"Restarting @ {datetime.utcnow().strftime('%H:%M:%s')}", channel=self.bot_logs, rqst_by=False)
         print("\nRestarting...\n")
         await self.bot.suicide()
         execl(executable, 'python "' + "".join(argv) + '"')
@@ -62,8 +62,8 @@ class Admin:
     async def suicide(self, ctx):
         """Owner of this bot only command; Shutdown the bot"""
         if ctx.channel != self.bot_logs:
-            await ctx.success(datetime.utcnow().strftime(self.bot.time_format), "Shutting Down:", rqst_by=False, timestamp=False)
-        await ctx.success(datetime.utcnow().strftime(self.bot.time_format), "Shutting Down:", channel=self.bot_logs, rqst_by=False, timestamp=False)
+            await ctx.success(None, f"Shutting Down @ {datetime.utcnow().strftime('%H:%M:%s')}", rqst_by=False)
+        await ctx.success(None, f"Shutting Down @ {datetime.utcnow().strftime('%H:%M:%s')}", channel=self.bot_logs, rqst_by=False)
         print("\nShutting Down...\n")
         await self.bot.suicide()
 
