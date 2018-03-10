ce14d3b2be28dc6b8a35bde8d4e760326eb3991f
Modelmat <Modelmat@users.noreply.github.com>
2018-03-10 12:21:38 +1100

    fix indent issue

--- main.py
+++ main.py
@@ -75,8 +75,8 @@ async def load_extensions(bots):
 async def on_connect():
     await bot.update_dbots_server_count(dbl_token)
     with async_timeout.timeout(20):
-            async with bot.session.get("https://api.github.com/repos/Modelmat/discord.py/commits/rewrite") as r:
-                bot.latest_commit = "g" + (await r.json())['sha'][:7]
+        async with bot.session.get("https://api.github.com/repos/Modelmat/discord.py/commits/rewrite") as r:
+            bot.latest_commit = "g" + (await r.json())['sha'][:7]
     await bot.pull_remotes()
 
 @bot.event
