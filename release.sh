32789e24132bcc60bbedf74b460fd002871ee077
Modelmat <modelmat@outlook.com.au>
2018-09-23 16:27:28 +1000

    Add a commit message so release.sh is fully automated.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 git checkout release
 git pull --no-edit
-git merge master release
+git merge master release -m "Merge release to master"
 git push origin release
 git checkout master
